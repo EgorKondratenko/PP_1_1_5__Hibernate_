663312de01634919fdf0aae91bb218c7fdbb1ce8
EgorKondratenko <122119909+EgorKondratenko@users.noreply.github.com>
2023-03-27 23:56:37 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,10 +5,8 @@ import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
-
-import static jm.task.core.jdbc.util.Util.getSessionFactory;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
@@ -17,74 +15,64 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
-        session.beginTransaction();
-        String stringSql = "CREATE TABLE IF NOT EXISTS USERS "
-                + "(ID BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,"
-                + "NAME VARCHAR(45),"
-                + "LASTNAME VARCHAR(45) NOT NULL,"
-                + "AGE TINYINT NOT NULL)";
-        session.createSQLQuery(stringSql).addEntity(User.class);
-        session.getTransaction().commit();
+        try(Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            String stringSql = "CREATE TABLE IF NOT EXISTS USERS "
+                    + "(ID BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,"
+                    + "NAME VARCHAR(45),"
+                    + "LASTNAME VARCHAR(45) NOT NULL,"
+                    + "AGE TINYINT NOT NULL)";
+            session.createSQLQuery(stringSql).addEntity(User.class);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        String stringSql = "DROP TABLE IF EXISTS USERS";
-        session.createSQLQuery(stringSql).addEntity(User.class);
-        session.getTransaction().commit();
+        try(Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            String stringSql = "DROP TABLE IF EXISTS USERS";
+            session.createSQLQuery(stringSql).addEntity(User.class);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
-        Session session = Util.getSessionFactory().openSession();
-        session.beginTransaction();
-        User user = new User(name, lastName, age);
-        session.save(user);
-        System.out.printf("User с именем - %s  добавлен в базу данных\n", name);
-        session.getTransaction().commit();
-
-
-        //        Transaction transaction = null;
-//        try (Session session = getSessionFactory().openSession()) {
-////            User user = new User(name, lastName, age);
-//            transaction = session.beginTransaction();
-//            session.save(name);
-//            transaction.commit();
-//            System.out.format("User с именем - %s  добавлен в базу данных\n", name);
-//        } catch (Exception e) {
-//            transaction.rollback();
-//            e.printStackTrace();
-//        }
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            System.out.printf("User с именем - %s  добавлен в базу данных\n", name);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = Util.getSessionFactory().openSession();
-        session.beginTransaction();
-//        User user = session.get(User.class, id);
-        session.delete(session.get(User.class, id));
-        session.getTransaction().commit();
-
-//        Transaction transaction = null;
-//        Session session = getSessionFactory().openSession();
-//        transaction = session.beginTransaction();
-//        User userRemove = new User();
-//        session.delete(userRemove);
-//        transaction.commit();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        List<User> userList;
+
         try (Session session = Util.getSessionFactory().openSession()) {
             session.beginTransaction();
-            String stringSql = "FROM User";
-            userList = session.createQuery(stringSql, User.class).getResultList();
+            String stringSql = "from User";
+            List<User> userList = session.createQuery(stringSql, User.class).getResultList();
             session.getTransaction().commit();
             return userList;
         } catch (Exception e) {
@@ -94,15 +82,14 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
-        session.beginTransaction();
-        String stringSql = "DELETE FROM users";
-        session.createSQLQuery(stringSql).executeUpdate();
-        session.getTransaction().commit();
 
-//        Transaction transaction = null;
-//        try (Session session = getSessionFactory().openSession()) {
-//            transaction = session.beginTransaction();
-//        }
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            String stringSql = "DELETE FROM users";
+            session.createSQLQuery(stringSql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
